d16c0c8a0078c75c92da53884973d387e52fbbff
Juliana Amorim <juliana@croct.com>
2019-10-11 16:22:07 -0300

    misc

--- src/beacon.ts
+++ src/beacon.ts
@@ -92,3 +92,96 @@ export type Beacon = {
     payload: Payload
     version: string
 }
+
+export type ProductDetails = {
+    productId: string
+    productSku: string | null
+    name: string
+    category: string | null
+    brand: string | null
+    variant: string | null
+    displayPrice: number
+    originalPrice: number | null
+    url: string | null
+    imageUrl: string | null
+}
+
+export type CartItem = {
+    index: number | null
+    productDetails: ProductDetails
+    quantity: number | null
+    total: number
+    discount: number | null
+    coupon: string | null
+}
+
+export type Cart = {
+    cartId: string | null
+    currency: string
+    items: CartItem[]
+    subtotal: number | null
+    shippingPrice: number | null
+    taxes: { [key: string]: number | null }
+    costs: { [key: string]: number | null }
+    discount: number | null
+    total: number
+    coupon: string | null
+    lastUpdateTime: number
+}
+
+export type OrderItem = {
+    index: number | null
+    productDetails: ProductDetails
+    quantity: number | null
+    total: number
+    discount: number | null
+    coupon: string | null
+}
+
+export type Order = {
+    cartId: string | null
+    orderId: string
+    currency: string
+    items: OrderItem[]
+    subtotal: number | null
+    shippingPrice: number | null
+    taxes: { [key: string]: number | null }
+    costs: { [key: string]: number | null }
+    discount: number | null
+    total: number
+    coupon: string | null
+    paymentMethod: string | null
+    installments: number | null
+    status: 'placed' | 'paid' | 'completed'
+}
+
+export type ProductViewed = Payload & {
+    productDetails: ProductDetails
+}
+
+export type CartModified = {
+    cart: Cart
+}
+
+export type CartViewed = {
+    cart: Cart
+}
+
+export type CheckoutStarted = {
+    cart: Cart
+    orderId: string | null
+}
+
+export type OrderPlaced = {
+    order: Order
+}
+
+export type UserSignedUp = {
+    token: string
+    firstName: string | null
+    lastName: string | null
+    birthDate: string | null
+    gender: 'male' | 'female' | 'neutral' | null
+    email: string | null
+    phone: string | null
+}
