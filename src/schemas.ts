5d2e8df7536a231ab1b4c47775c34366970a5e52
Juliana Amorim <juliana@croct.com>
2019-10-12 12:42:41 -0300

    misc

--- src/schemas.ts
+++ src/schemas.ts
@@ -1,11 +1,10 @@
-import {BooleanType, ObjectType, StringType} from './validation';
+import {ArrayType, BooleanType, NumberType, ObjectType, StringType} from './validation';
 
-export var configurationSchema = new ObjectType({
-    additionalProperties: false,
+export const configurationSchema = new ObjectType({
     required: ['apiKey'],
     properties: {
         apiKey: new StringType({
-            pattern: /^[0-9a-f]{8}-(?:[0-9a-f]{4}-){3}[0-9a-f]{12}$/
+            format: 'uuid'
         }),
         storageNamespace: new StringType({
             pattern: /^[a-zA-Z_][a-zA-Z0-9_]*$/
@@ -19,5 +18,269 @@ export var configurationSchema = new ObjectType({
         }),
         debug: new BooleanType(),
         track: new BooleanType(),
+    }
+});
+
+export const productDetailSchema = new ObjectType({
+    required: ['name', 'displayPrice'],
+    properties: {
+        productId: new StringType({
+            minLength: 1,
+            maxLength: 50
+        }),
+        productSku: new StringType({
+            minLength: 1,
+            maxLength: 50
+        }),
+        name: new StringType({
+            minLength: 1,
+            maxLength: 200
+        }),
+        category: new StringType({
+            minLength: 1,
+            maxLength: 100
+        }),
+        brand: new StringType({
+            minLength: 1,
+            maxLength: 100
+        }),
+        variant: new StringType({
+            minLength: 1,
+            maxLength: 50
+        }),
+        displayPrice: new NumberType({
+            minimum: 0
+        }),
+        originalPrice: new NumberType({
+            minimum: 0
+        }),
+        url: new StringType({
+            format: 'url'
+        }),
+        imageUrl: new StringType({
+            format: 'url'
+        }),
+    }
+});
+
+export const cartItemSchema = new ObjectType({
+    required: ['product', 'quantity', 'total'],
+    properties: {
+        index: new NumberType({
+            minimum: 0
+        }),
+        product: productDetailSchema,
+        quantity: new NumberType({
+            minimum: 1
+        }),
+        total: new NumberType({
+            minimum: 0
+        }),
+        discount: new NumberType({
+            minimum: 0
+        }),
+        coupon: new StringType({
+            minLength: 1,
+            maxLength: 50
+        }),
+    }
+});
+
+export const cartSchema = new ObjectType({
+    required: ['currency', 'items', 'total', 'lastUpdateTime'],
+    properties: {
+        cartId: new StringType({
+            minLength: 1,
+            maxLength: 50
+        }),
+        currency: new StringType({
+            format: 'currency'
+        }),
+        items: new ArrayType({
+            items: cartItemSchema,
+            minItems: 1
+        }),
+        subtotal: new NumberType({
+            minimum: 0
+        }),
+        shippingPrice: new NumberType({
+            minimum: 0
+        }),
+        taxes: new ObjectType({
+            additionalProperties: new NumberType({}),
+            minProperties: 1
+        }),
+        costs: new ObjectType({
+            additionalProperties: new NumberType({}),
+            minProperties: 1
+        }),
+        discount: new NumberType({
+            minimum: 0
+        }),
+        total: new NumberType({
+            minimum: 0
+        }),
+        coupon: new StringType({
+            minLength: 1,
+            maxLength: 50
+        }),
+        lastUpdateTime: new NumberType({})
+    }
+});
+
+export const orderItemSchema = new ObjectType({
+    required: ['product', 'quantity', 'total'],
+    properties: {
+        index: new NumberType({
+            minimum: 0
+        }),
+        product: productDetailSchema,
+        quantity: new NumberType({
+            minimum: 1
+        }),
+        total: new NumberType({
+            minimum: 0
+        }),
+        discount: new NumberType({
+            minimum: 0
+        }),
+        coupon: new StringType({
+            minLength: 1,
+            maxLength: 50
+        }),
+    }
+});
+
+export const orderSchema = new ObjectType({
+    required: ['orderId', 'currency', 'items', 'total'],
+    properties: {
+        cartId: new StringType({
+            minLength: 1,
+            maxLength: 50
+        }),
+        orderId: new StringType({
+            minLength: 1,
+            maxLength: 50
+        }),
+        currency: new StringType({
+            format: 'currency'
+        }),
+        items: new ArrayType({
+            items: orderItemSchema,
+            minItems: 1
+        }),
+        subtotal: new NumberType({
+            minimum: 0
+        }),
+        shippingPrice: new NumberType({
+            minimum: 0
+        }),
+        taxes: new ObjectType({
+            additionalProperties: new NumberType({}),
+            minProperties: 1
+        }),
+        costs: new ObjectType({
+            additionalProperties: new NumberType({}),
+            minProperties: 1
+        }),
+        discount: new NumberType({
+            minimum: 0
+        }),
+        total: new NumberType({
+            minimum: 0
+        }),
+        coupon: new StringType({
+            minLength: 1,
+            maxLength: 50
+        }),
+        paymentMethod: new StringType({
+            minLength: 1,
+            maxLength: 50
+        }),
+        installments: new NumberType({
+            minimum: 1
+        }),
+        status: new StringType({
+            enumeration: [
+                'placed',
+                'paid',
+                'complete'
+            ]
+        })
+    }
+});
+
+export const cartModifiedSchema = new ObjectType({
+    required: ['cart'],
+    properties: {
+        cart: cartSchema
+    }
+});
+
+export const cartViewedSchema = new ObjectType({
+    required: ['cart'],
+    properties: {
+        cart: cartSchema
+    }
+});
+
+export const checkoutStartedSchema = new ObjectType({
+    required: ['cart'],
+    properties: {
+        cart: cartSchema,
+        orderId: new StringType({
+            minLength: 1,
+            maxLength: 50
+        }),
+    }
+});
+
+export const orderPlacedSchema = new ObjectType({
+    required: ['order'],
+    properties: {
+        order: orderSchema
+    }
+});
+
+export const productViewedSchema = new ObjectType({
+    required: ['productDetails'],
+    properties: {
+        productDetails: productDetailSchema
+    }
+});
+
+export const userSignedUpSchema = new ObjectType({
+    required: ['token'],
+    properties: {
+        "token": new StringType({
+            minLength: 1,
+            maxLength: 100
+        }),
+        "firstName": new StringType({
+            minLength: 1,
+            maxLength: 100
+        }),
+        "lastName": new StringType({
+            minLength: 1,
+            maxLength: 100
+        }),
+        "birthDate": new StringType({
+            format: 'date'
+        }),
+        gender: new StringType({
+            enumeration: [
+                'male',
+                'female',
+                'neutral'
+            ]
+        }),
+        email: new StringType({
+            minLength: 1,
+            maxLength: 100
+        }),
+        phone: new StringType({
+            minLength: 1,
+            maxLength: 50
+        }),
     }
 });
