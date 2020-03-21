622b454f281fbba24cf4a5a3084b865fcfbddd98
julianaamorim <juliana@croct.com>
2020-03-21 17:36:32 -0300

    Narrow types allowed by operation scheme
    Signed-off-by: julianaamorim <juliana@croct.com>

--- test/schemas/operationSchemas.test.ts
+++ test/schemas/operationSchemas.test.ts
@@ -1,4 +1,3 @@
-import {TypeSchema} from '../../src/validation';
 import {
     addOperation,
     clearOperation,
@@ -10,114 +9,39 @@ import {
     unsetOperation,
 } from '../../src/schema/operationSchemas';
 
-describe('An operation schema', () => {
+const simpleArray = [1, 1.2, null, 'foo', true];
+
+const simpleMap = {
+    a: 1,
+    b: 1.2,
+    c: null,
+    d: 'foo',
+    e: true,
+};
+
+const complexMap = {
+    a: 1,
+    b: 1.2,
+    c: null,
+    d: 'foo',
+    e: true,
+    f: simpleArray,
+    g: simpleMap,
+};
+
+describe('An add operation schema', () => {
     test.each([
-        [
-            {
-                path: 'foo',
-                value: 'bar',
-            },
-            addOperation,
-        ],
-        [
-            {
-                path: 'foo',
-                value: ['bar'],
-            },
-            addOperation,
-        ],
-        [
-            {
-                path: 'foo',
-                value: {foo: 'bar'},
-            },
-            addOperation,
-        ],
-        [
-            {
-                path: 'foo',
-                value: 'bar',
-            },
-            setOperation,
-        ],
-        [
-            {
-                path: 'foo',
-                value: ['bar'],
-            },
-            setOperation,
-        ],
-        [
-            {
-                path: 'foo',
-                value: {foo: 'bar'},
-            },
-            setOperation,
-        ],
-        [
-            {
-                path: 'foo',
-                value: 'bar',
-            },
-            combineOperation,
-        ],
-        [
-            {
-                path: 'foo',
-                value: ['bar'],
-            },
-            combineOperation,
-        ],
-        [
-            {
-                path: 'foo',
-                value: {foo: 'bar'},
-            },
-            combineOperation,
-        ],
-        [
-            {
-                path: 'foo',
-                value: ['bar'],
-            },
-            mergeOperation,
-        ],
-        [
-            {
-                path: 'foo',
-                value: {foo: 'bar'},
-            },
-            mergeOperation,
-        ],
-        [
-            {
-                path: 'foo',
-                value: 1,
-            },
-            incrementOperation,
-        ],
-        [
-            {
-                path: 'foo',
-                value: 2,
-            },
-            decrementOperation,
-        ],
-        [
-            {
-                path: 'foo',
-            },
-            unsetOperation,
-        ],
-        [
-            {
-                path: 'foo',
-            },
-            clearOperation,
-        ],
-    ])('should allow %s', (value: object, schema: TypeSchema) => {
+        [1],
+        [1.2],
+        [null],
+        ['foo'],
+        [true],
+        [simpleArray],
+        [simpleMap],
+        [complexMap],
+    ])('should allow value %s', (value: any) => {
         function validate(): void {
-            schema.validate(value);
+            addOperation.validate({path: 'foo', value: value});
         }
 
         expect(validate).not.toThrowError(Error);
@@ -125,59 +49,295 @@ describe('An operation schema', () => {
 
     test.each([
         [
-            {
-                path: 'foo',
-                value: /not-a-json-value/,
-            },
-            addOperation,
-            'Expected a JSON value at path \'/value\', actual RegExp.',
+            [['foo']],
+            'Expected a JSON primitive at path \'/value/0\', actual array.',
         ],
         [
-            {
-                path: 'foo',
-                value: /not-a-json-value/,
-            },
-            setOperation,
-            'Expected a JSON value at path \'/value\', actual RegExp.',
+            {map: {map: {foo: 1}}},
+            'Expected a JSON primitive at path \'/value/map/map\', actual Object.',
         ],
         [
-            {
-                path: 'foo',
-                value: /not-a-json-value/,
-            },
-            combineOperation,
-            'Expected a JSON value at path \'/value\', actual RegExp.',
+            {map: {list: ['foo']}},
+            'Expected a JSON primitive at path \'/value/map/list\', actual array.',
         ],
         [
-            {
-                path: 'foo',
-                value: 'bar',
-            },
-            mergeOperation,
-            'Expected value of type array or object at path \'/value\', actual string.',
+            /not-a-json-value/,
+            'Expected value of type null, number, string, boolean, array or object at path \'/value\', actual RegExp.',
         ],
-        [
-            {
-                path: 'foo',
-                value: 'bar',
-            },
-            incrementOperation,
-            'Expected value of type number at path \'/value\', actual string.',
-        ],
-        [
-            {
-                path: 'foo',
-                value: 'bar',
-            },
-            decrementOperation,
-            'Expected value of type number at path \'/value\', actual string.',
-        ],
-    ])('should not allow %s', (value: object, schema: TypeSchema, message: string) => {
+    ])('should not allow value %s', (value: any, message: string) => {
         function validate(): void {
-            schema.validate(value);
+            addOperation.validate({path: 'foo', value: value});
         }
 
         expect(validate).toThrow(Error);
         expect(validate).toThrow(message);
+    });
+
+    test('should fail if path is invalid', () => {
+        function validate(): void {
+            addOperation.validate({path: 'foo.', value: 'bar'});
+        }
+
+        expect(validate).toThrow(Error);
+        expect(validate).toThrow('Invalid pointer format at path \'/path\'.');
+    });
+});
+
+describe('A set operation schema', () => {
+    test.each([
+        [1],
+        [1.2],
+        [null],
+        ['foo'],
+        [true],
+        [simpleArray],
+        [simpleMap],
+        [complexMap],
+    ])('should allow value %s', (value: any) => {
+        function validate(): void {
+            setOperation.validate({path: 'foo', value: value});
+        }
+
+        expect(validate).not.toThrowError(Error);
+    });
+
+    test.each([
+        [
+            [['foo']],
+            'Expected a JSON primitive at path \'/value/0\', actual array.',
+        ],
+        [
+            {map: {map: {foo: 1}}},
+            'Expected a JSON primitive at path \'/value/map/map\', actual Object.',
+        ],
+        [
+            {map: {list: ['foo']}},
+            'Expected a JSON primitive at path \'/value/map/list\', actual array.',
+        ],
+        [
+            /not-a-json-value/,
+            'Expected value of type null, number, string, boolean, array or object at path \'/value\', actual RegExp.',
+        ],
+    ])('should not allow value %s', (value: any, message: string) => {
+        function validate(): void {
+            setOperation.validate({path: 'foo', value: value});
+        }
+
+        expect(validate).toThrow(Error);
+        expect(validate).toThrow(message);
+    });
+
+    test('should fail if path is invalid', () => {
+        function validate(): void {
+            setOperation.validate({path: 'foo.', value: 'bar'});
+        }
+
+        expect(validate).toThrow(Error);
+        expect(validate).toThrow('Invalid pointer format at path \'/path\'.');
+    });
+});
+
+describe('A combine operation schema', () => {
+    test.each([
+        [1],
+        [1.2],
+        [null],
+        ['foo'],
+        [true],
+        [simpleArray],
+        [simpleMap],
+        [complexMap],
+    ])('should allow value %s', (value: any) => {
+        function validate(): void {
+            combineOperation.validate({path: 'foo', value: value});
+        }
+
+        expect(validate).not.toThrowError(Error);
+    });
+
+    test.each([
+        [
+            [['foo']],
+            'Expected a JSON primitive at path \'/value/0\', actual array.',
+        ],
+        [
+            {map: {map: {foo: 1}}},
+            'Expected a JSON primitive at path \'/value/map/map\', actual Object.',
+        ],
+        [
+            {map: {list: ['foo']}},
+            'Expected a JSON primitive at path \'/value/map/list\', actual array.',
+        ],
+        [
+            /not-a-json-value/,
+            'Expected value of type null, number, string, boolean, array or object at path \'/value\', actual RegExp.',
+        ],
+    ])('should not allow value %s', (value: any, message: string) => {
+        function validate(): void {
+            combineOperation.validate({path: 'foo', value: value});
+        }
+
+        expect(validate).toThrow(Error);
+        expect(validate).toThrow(message);
+    });
+
+    test('should fail if path is invalid', () => {
+        function validate(): void {
+            combineOperation.validate({path: 'foo.', value: 'bar'});
+        }
+
+        expect(validate).toThrow(Error);
+        expect(validate).toThrow('Invalid pointer format at path \'/path\'.');
+    });
+});
+
+describe('A merge operation schema', () => {
+    test.each([
+        [simpleArray],
+        [simpleMap],
+        [complexMap],
+    ])('should allow value %s', (value: any) => {
+        function validate(): void {
+            mergeOperation.validate({path: 'foo', value: value});
+        }
+
+        expect(validate).not.toThrowError(Error);
+    });
+
+    test.each([
+        [1, 'Expected value of type array or object at path \'/value\', actual integer.'],
+        [1.2, 'Expected value of type array or object at path \'/value\', actual number.'],
+        [null, 'Expected value of type array or object at path \'/value\', actual null.'],
+        ['foo', 'Expected value of type array or object at path \'/value\', actual string.'],
+        [true, 'Expected value of type array or object at path \'/value\', actual boolean.'],
+        [
+            [['foo']],
+            'Expected a JSON primitive at path \'/value/0\', actual array.',
+        ],
+        [
+            {map: {map: {foo: 1}}},
+            'Expected a JSON primitive at path \'/value/map/map\', actual Object.',
+        ],
+        [
+            {map: {list: ['foo']}},
+            'Expected a JSON primitive at path \'/value/map/list\', actual array.',
+        ],
+        [/not-a-json-value/, 'Expected value of type array or object at path \'/value\', actual RegExp.'],
+    ])('should not allow value %s', (value: any, message: string) => {
+        function validate(): void {
+            mergeOperation.validate({path: 'foo', value: value});
+        }
+
+        expect(validate).toThrow(Error);
+        expect(validate).toThrow(message);
+    });
+
+    test('should fail if path is invalid', () => {
+        function validate(): void {
+            mergeOperation.validate({path: 'foo.', value: 'bar'});
+        }
+
+        expect(validate).toThrow(Error);
+        expect(validate).toThrow('Invalid pointer format at path \'/path\'.');
+    });
+});
+
+describe('An increment operation schema', () => {
+    test.each([
+        [1],
+        [1.2],
+    ])('should allow value %s', (value: any) => {
+        function validate(): void {
+            incrementOperation.validate({path: 'foo', value: value});
+        }
+
+        expect(validate).not.toThrowError(Error);
+    });
+
+    test.each([
+        [null, 'Expected value of type number at path \'/value\', actual null.'],
+        ['foo', 'Expected value of type number at path \'/value\', actual string.'],
+        [true, 'Expected value of type number at path \'/value\', actual boolean.'],
+        [simpleArray, 'Expected value of type number at path \'/value\', actual array.'],
+        [simpleMap, 'Expected value of type number at path \'/value\', actual Object.'],
+        [complexMap, 'Expected value of type number at path \'/value\', actual Object.'],
+        [/not-a-json-value/, 'Expected value of type number at path \'/value\', actual RegExp.'],
+    ])('should not allow value %s', (value: any, message: string) => {
+        function validate(): void {
+            incrementOperation.validate({path: 'foo', value: value});
+        }
+
+        expect(validate).toThrow(Error);
+        expect(validate).toThrow(message);
+    });
+
+    test('should fail if path is invalid', () => {
+        function validate(): void {
+            incrementOperation.validate({path: 'foo.', value: 1});
+        }
+
+        expect(validate).toThrow(Error);
+        expect(validate).toThrow('Invalid pointer format at path \'/path\'.');
+    });
+});
+
+describe('A decrement operation schema', () => {
+    test.each([
+        [1],
+        [1.2],
+    ])('should allow value %s', (value: any) => {
+        function validate(): void {
+            decrementOperation.validate({path: 'foo', value: value});
+        }
+
+        expect(validate).not.toThrowError(Error);
+    });
+
+    test.each([
+        [null, 'Expected value of type number at path \'/value\', actual null.'],
+        ['foo', 'Expected value of type number at path \'/value\', actual string.'],
+        [true, 'Expected value of type number at path \'/value\', actual boolean.'],
+        [simpleArray, 'Expected value of type number at path \'/value\', actual array.'],
+        [simpleMap, 'Expected value of type number at path \'/value\', actual Object.'],
+        [complexMap, 'Expected value of type number at path \'/value\', actual Object.'],
+        [/not-a-json-value/, 'Expected value of type number at path \'/value\', actual RegExp.'],
+    ])('should not allow value %s', (value: any, message: string) => {
+        function validate(): void {
+            decrementOperation.validate({path: 'foo', value: value});
+        }
+
+        expect(validate).toThrow(Error);
+        expect(validate).toThrow(message);
+    });
+
+    test('should fail if path is invalid', () => {
+        function validate(): void {
+            decrementOperation.validate({path: 'foo.', value: 1});
+        }
+
+        expect(validate).toThrow(Error);
+        expect(validate).toThrow('Invalid pointer format at path \'/path\'.');
+    });
+});
+
+describe('A clear operation schema', () => {
+    test('should fail if path is invalid', () => {
+        function validate(): void {
+            clearOperation.validate({path: 'foo.'});
+        }
+
+        expect(validate).toThrow(Error);
+        expect(validate).toThrow('Invalid pointer format at path \'/path\'.');
+    });
+});
+
+describe('An unset operation schema', () => {
+    test('should fail if path is invalid', () => {
+        function validate(): void {
+            unsetOperation.validate({path: 'foo.'});
+        }
+
+        expect(validate).toThrow(Error);
+        expect(validate).toThrow('Invalid pointer format at path \'/path\'.');
     });
 });
