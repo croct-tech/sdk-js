89098f4d5b99e8b13c7842ef581f4ebf55d40e6e
julianaamorim <juliana@croct.com>
2020-03-21 12:39:46 -0300

    Add schema and tests for pointer and identifier

--- test/validation/stringType.test.ts
+++ test/validation/stringType.test.ts
@@ -19,6 +19,71 @@ describe('A string type', () => {
     });
 
     test.each([
+        ['bd70aced-f238-4a06-b49d-0c96fe10c4f8', 'uuid'],
+        ['2015-08-31', 'date'],
+        ['http://www.foo.com.br', 'url'],
+        ['_', 'identifier'],
+        ['a', 'identifier'],
+        ['A', 'identifier'],
+        ['a_', 'identifier'],
+        ['a1', 'identifier'],
+        ['aa', 'identifier'],
+        ['aA', 'identifier'],
+        ['a.b', 'pointer'],
+        ['a[0].b', 'pointer'],
+        ['ab12_[0].cd34_', 'pointer'],
+        ['abc[0].dfg[1].h', 'pointer'],
+        ['abc[0].dfg[1]', 'pointer'],
+        ['abc[0][1].foo', 'pointer'],
+        ['_[0][1]._', 'pointer'],
+        ['[0][1].foo', 'pointer'],
+        ['_._', 'pointer'],
+    ])('should allow %s with format %s', (value: string, format: string) => {
+        function validate(): void {
+            new StringType({format: format}).validate(value);
+        }
+
+        expect(validate).not.toThrowError(Error);
+    });
+
+    test.each([
+        ['bd70acedf2384a06b49d0c96fe10c4f8', 'uuid'],
+        ['bd70aced-zzzz-4a06-b49d-0c96fe10c4f8', 'uuid'],
+        ['20150831', 'date'],
+        ['2015-08', 'date'],
+        ['08-31', 'date'],
+        ['foo.com', 'url'],
+        ['', 'identifier'],
+        ['@foo', 'identifier'],
+        ['a@#$', 'identifier'],
+        ['', 'pointer'],
+        ['a.', 'pointer'],
+        ['.a', 'pointer'],
+        ['a[].b', 'pointer'],
+        ['123', 'pointer'],
+        ['123[0].cd34_', 'pointer'],
+        ['.h', 'pointer'],
+        ['[]', 'pointer'],
+        ['[][]', 'pointer'],
+        ['[0].1', 'pointer'],
+        ['1.a', 'pointer'],
+        ['a.1', 'pointer'],
+        ['abc[a]', 'pointer'],
+        ['abc[-1]', 'pointer'],
+        ['@#$', 'pointer'],
+        ['.', 'pointer'],
+        ['[0', 'pointer'],
+        ['0]', 'pointer'],
+    ])('should not allow %s with format %s', (value: string, format: string) => {
+        function validate(): void {
+            new StringType({format: format}).validate(value);
+        }
+
+        expect(validate).toThrow(Error);
+        expect(validate).toThrow(`Invalid ${format} format at path '/'.`);
+    });
+
+    test.each([
         ['foo', new StringType({minLength: 2})],
         ['foo', new StringType({maxLength: 4})],
         ['foo', new StringType({enumeration: ['foo']})],
@@ -49,11 +114,7 @@ describe('A string type', () => {
         ],
         ['bar', new StringType({pattern: /fo+/}), "Invalid format at path '/'."],
         ['foo', new StringType({format: 'url'}), "Invalid url format at path '/'."],
-    ])('should not allow %s with %o', (
-        value: any,
-        type: StringType,
-        message: string,
-    ) => {
+    ])('should not allow %s with %o', (value: any, type: StringType, message: string) => {
         function validate(): void {
             type.validate(value);
         }
