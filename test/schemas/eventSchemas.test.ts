c7aa5dfa53d325f4c2e4816cb99de8f59fdceaf8
Denis Rossati Ramos <denis.rossatiramos@gmail.com>
2022-01-19 11:20:34 -0300

    adding tests that check some valid or invalid payloads

--- test/schemas/eventSchemas.test.ts
+++ test/schemas/eventSchemas.test.ts
@@ -10,6 +10,7 @@ import {
     goalCompleted,
     interestShown,
     postViewed,
+    linkOpened,
 } from '../../src/schema';
 import {Optional} from '../../src/utilityTypes';
 
@@ -482,6 +483,42 @@ describe('The "postViewed" payload schema', () => {
     ])('should not allow %s', (value: Record<string, unknown>, message: string) => {
         function validate(): void {
             postViewed.validate(value);
+        }
+
+        expect(validate).toThrow(Error);
+        expect(validate).toThrow(message);
+    });
+});
+
+describe('The "linkOpened" payload schema', () => {
+    test.each([
+        [{
+            link: 'http://www.foo.com.br',
+        }],
+    ])('should allow %s', (value: Record<string, unknown>) => {
+        function validate(): void {
+            linkOpened.validate(value);
+        }
+
+        expect(validate).not.toThrow(Error);
+    });
+
+    test.each([
+        [
+            {},
+            'Missing property \'/link\'.',
+        ],
+        [
+            {link: null},
+            'Expected value of type string at path \'/link\', actual null.',
+        ],
+        [
+            {link: ''},
+            'Invalid url format at path \'/link\'.',
+        ],
+    ])('shout not allow %s', (value: Record<string, unknown>, message: string) => {
+        function validate(): void {
+            linkOpened.validate(value);
         }
 
         expect(validate).toThrow(Error);
