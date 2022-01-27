b7501280f56019804d38bdc4bd170ec0cdddd04e
Marcos Passos <marcos@croct.com>
2022-01-27 19:26:46 -0300

    Implement link opened event

--- src/validation/stringType.ts
+++ src/validation/stringType.ts
@@ -28,7 +28,7 @@ const FORMAT: {[key: string]: Format} = {
 
         return true;
     },
-    'uri-reference': function uri(value: string): boolean {
+    'uri-reference': function uriReference(value: string): boolean {
         try {
             // This simplistic approach covers the most common cases
             // without inflating the library with an RFC 3986-compliant parser.
