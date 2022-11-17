9efbe2929a9e1cd46dc2b7f7be2893edf067e3cc
Luiz Ferraz <luiz@lferraz.com>
2022-11-17 19:18:51 -0300

    Remove babel and re-add ts-jest

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
     testEnvironment: 'jsdom',
+    preset: 'ts-jest',
     testMatch: ['<rootDir>/test/**/*.test.ts'],
-    transformIgnorePatterns: [
-        'node_modules/(?!@croct/.*)',
-    ],
+    setupFilesAfterEnv: ['jest-extended/all'],
 };
