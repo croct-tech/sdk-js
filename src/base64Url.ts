66447a5ec3ba00909596b1c3610770adcc6df1ff
Marcos Passos <marcos@croct.com>
2024-05-06 17:34:59 -0600

    Make compatible with node

--- src/base64Url.ts
+++ src/base64Url.ts
@@ -11,17 +11,17 @@ function base64Escape(value: string): string {
 
 export function base64UrlEncode(value: string): string {
     return base64Escape(
-        window.btoa(
-            window.encodeURIComponent(value)
+        btoa(
+            encodeURIComponent(value)
                 .replace(/%([0-9A-F]{2})/g, (_, p1) => String.fromCharCode(Number.parseInt(p1, 16))),
         ),
     );
 }
 
 export function base64UrlDecode(value: string): string {
-    return window.decodeURIComponent(
+    return decodeURIComponent(
         Array.prototype.map.call(
-            window.atob(base64Unescape(value)),
+            atob(base64Unescape(value)),
             (char: string) => `%${(`00${char.charCodeAt(0).toString(16)}`).slice(-2)}`,
         ).join(''),
     );
