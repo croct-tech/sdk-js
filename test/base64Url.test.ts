dbe9cbaa80041e9ae0c8090e816e2e96a78ff3fd
Luiz Ferraz <luiz.ferraz@croct.com>
2024-05-06 22:05:14 -0300

    WIP

--- test/base64Url.test.ts
+++ test/base64Url.test.ts
@@ -1,4 +1,4 @@
-import {base64UrlDecode, base64UrlEncode} from '../src/base64Url';
+import { base64UrlDecode, base64UrlEncode } from '../src/base64Url';
 
 describe('A base64 URL encoder/decoder function', () => {
     const encodeTests: Array<[string]> = [
@@ -26,5 +26,13 @@ describe('A base64 URL encoder/decoder function', () => {
 
     it.each(encodeTests)('should encode "%s" as "%s"', (decoded: string) => {
         expect(base64UrlDecode(base64UrlEncode(decoded))).toBe(decoded);
+    });
+
+    it('should encode a reference string', () => {
+        const original = atob('qacWVddeO9x66kkymtPHTvh4hHnhdJ437m1HOSHh1od+aA94EB9bK67QFjaMo4PVxjZxhf2x4dHSCLievgGKpQ==');
+
+        const encoded = base64UrlEncode(original);
+
+        expect(encoded).toEqual('qacWVddeO9x66kkymtPHTvh4hHnhdJ437m1HOSHh1od-aA94EB9bK67QFjaMo4PVxjZxhf2x4dHSCLievgGKpQ');
     });
 });
