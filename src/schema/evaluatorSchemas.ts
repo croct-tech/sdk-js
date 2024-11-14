83fad073adc50056b87478b9d294afac5a7dbb5b
Denis Rossati <denis.rossatiramos@gmail.com>
2024-11-14 11:34:02 -0300

    Rework evaluation attribute schema validation (#441)

--- src/schema/evaluatorSchemas.ts
+++ src/schema/evaluatorSchemas.ts
@@ -1,4 +1,39 @@
-import {ObjectType, NumberType, JsonObjectType} from '../validation';
+import {
+    ObjectType,
+    NumberType,
+    JsonObjectType,
+    UnionType,
+    StringType,
+    NullType,
+    BooleanType,
+    TypeSchema,
+    ArrayType,
+} from '../validation';
+
+function createJsonSchema(maximumDepth: number): TypeSchema {
+    return new UnionType(
+        new NullType(),
+        new NumberType(),
+        new BooleanType(),
+        new StringType({maxLength: 255}),
+        ...(maximumDepth > 1
+            ? [
+                new JsonObjectType({
+                    propertyNames: new UnionType(
+                        new NumberType(),
+                        new StringType({
+                            minLength: 1,
+                            maxLength: 50,
+                        }),
+                    ),
+                    properties: createJsonSchema(maximumDepth - 1),
+                }),
+                new ArrayType({items: createJsonSchema(maximumDepth - 1)}),
+            ]
+            : []
+        ),
+    );
+}
 
 export const evaluationOptionsSchema = new ObjectType({
     properties: {
@@ -6,6 +41,12 @@ export const evaluationOptionsSchema = new ObjectType({
             integer: true,
             minimum: 0,
         }),
-        attributes: new JsonObjectType(),
+        attributes: new JsonObjectType({
+            propertyNames: new StringType({
+                minLength: 1,
+                maxLength: 50,
+            }),
+            properties: createJsonSchema(5),
+        }),
     },
 });
