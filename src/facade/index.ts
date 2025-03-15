2fd9e7f35c84048c864b6e83dbd5f2ec7c76bd12
Marcos Passos <marcos@croct.com>
2025-03-15 12:33:43 -0300

    Fix import paths

--- src/facade/index.ts
+++ src/facade/index.ts
@@ -1,4 +1,4 @@
-export {EvaluatorFacade, ContextFactory, MinimalContextFactory, TabContextFactory} from './evaluatorFacade';
+export {EvaluatorFacade, type ContextFactory, MinimalContextFactory, TabContextFactory} from './evaluatorFacade';
 export {SdkFacade} from './sdkFacade';
 export {SessionFacade} from './sessionFacade';
 export {SessionPatch} from './sessionPatch';
